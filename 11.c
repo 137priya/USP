a18db8e20caddf824fa0769941dad5293cb67bfb
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-05-26 21:15:30 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -6,17 +6,17 @@
 #include<time.h>
 #include<fcntl.h>
 int main(){
-    struct utimbuf times;
-    struct stat mystat;
-    umask(0600);
-    if(creat("source2.txt",0666)<0){
-        printf("Error");
-        return 0;
-    }
-    stat("source2.txt",&mystat);
-    times.actime=mystat.st_atime;
-    times.modtime=mystat.st_mtime;
-    utime("source2.txt",&times);
-    printf("%s %s",ctime(&times.actime),ctime(&times.modtime));
-    printf("%s %s",ctime(&mystat.st_atime),ctime(&mystat.st_mtime));
+struct utimbuf times;
+struct stat mystat;
+umask(0600);
+if(creat("source2.txt",0666)<0){
+printf("Error");
+return 0;
+}
+stat("source2.txt",&mystat);
+times.actime=mystat.st_atime;
+times.modtime=mystat.st_mtime;
+utime("source2.txt",&times);
+printf("%s %s",ctime(&times.actime),ctime(&times.modtime));
+printf("%s %s",ctime(&mystat.st_atime),ctime(&mystat.st_mtime));
 }
