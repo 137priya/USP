50663b452b6feefeee6ba8ce490f26bea5248af5
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-05-26 20:36:10 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -3,10 +3,12 @@
 #include <sys/wait.h>
 int main(void)
 {pid_t pid;
-if ((pid = fork()) < 0) {
+if ((pid = fork()) < 0) 
+{
 err_sys("fork error");
 }
-else if (pid == 0) { /* first child */
+else if (pid == 0) 
+{ /* first child */
 if ((pid = fork()) < 0)
 err_sys("fork error");
   
